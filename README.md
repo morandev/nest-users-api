b4310f32ec743e0c96016e1fe30d12190d47a1a2
Morandev <moranfcnddev@gmail.com>
2023-02-23 05:48:17 -0300

    set: type orm config

---
+++ README.md
@@ -0,0 +1,4 @@
+### Reference Links
+
+- https://docs.nestjs.com/recipes/sql-typeorm
+- https://docs.nestjs.com/techniques/database
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
-
+//1. import nest js typeorm
+import { TypeOrmModule } from '@nestjs/typeorm';
+//dirname / * * dentro de cualquier carpeta, / * . entity.ts hay un archivo .. vas a cargarlo
+//__dirname + '/**/*'
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      username: 'morandev',
+      password: 'morandev',
+      host: 'localhost',
+      port: 3306,
+      database: 'db_users',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
